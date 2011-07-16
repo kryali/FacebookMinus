e6a3b1dee92a647c3c2a8365776dd8feabd6cf5e
Kiran Ryali <kiran.ryali@gmail.com>
2011-07-15 22:37:43 -0700

    fb stuff in ther now

--- public/javascripts/fb.js
+++ public/javascripts/fb.js
@@ -4,3 +4,28 @@ FB.init({
   cookie : true, // enable cookies to allow the server to access the session
   xfbml  : true  // parse XFBML
 });
+
+function fb_login(){·
+  FB.getLoginStatus(function(response) {
+    if(response.session) {
+      FB.api('/me', function(user) {
+        if(user != null) {
+          console.log(user);
+        }
+      });
+    }
+  });
+
+  //Actions to take upon the user logging in
+  FB.Event.subscribe('auth.login', function(response) {
+    if(response.session) {
+      FB.api('/me', function(user) {
+       if(user != null) {
+          console.log(user);
+        }
+      });
+    } else {
+      //Login failed??
+    }
+  });
+}
