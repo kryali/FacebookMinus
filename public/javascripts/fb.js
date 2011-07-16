862315ce3399ea69cc6a8af9e298f101b04fa683
Kiran Ryali <kiran.ryali@gmail.com>
2011-07-15 22:18:16 -0700

    added home controlelr

---
+++ public/javascripts/fb.js
@@ -0,0 +1,6 @@
+FB.init({
+  appId  : '205668742817032',
+  status : true, // check login status
+  cookie : true, // enable cookies to allow the server to access the session
+  xfbml  : true  // parse XFBML
+});
