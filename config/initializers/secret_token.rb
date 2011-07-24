2e9c62efb004b83f7aa0bd69eca10f99a9144e35
Kiran Ryali <kiran.ryali@gmail.com>
2011-07-24 16:36:29 -0700

    rm

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FacebookMinus::Application.config.secret_token = '9bee435c9ac140309daa532b556c7d26a34ce8058271687eb216659852361ecd0ddf6fe44cab94cf2275e3cf8c2172e58529ef4d0d9e716d1db8b6a3574748cb'
